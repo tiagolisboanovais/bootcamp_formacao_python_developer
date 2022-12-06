0764b1dbef6ffed0589c4e6e07d67267faf87e88
tiagolisboanovais <tiagolisboanovais@outlook.com>
2022-12-06 07:25:44 -0300

    Adicionando pasta do modulo 1.

---
+++ 1_Fundamentos_de_Python/1_Ambiente_de_Desenvolvimento_e_Primeiros_Passos_com_Python/primeiro_programa.py
@@ -0,0 +1,5 @@
+print("Hello world!")
+
+print("Olá mundo!")
+
+print("Oi, seja bem vindo ao curso de Python!")
---
+++ 1_Fundamentos_de_Python/2_Conhecendo_a_Linguagem_de_Programacao_Python/tiposdedados.py
@@ -0,0 +1,14 @@
+#Tipos de dados
+
+letra = str(1)
+num_int = int("1")
+num_float = float ("1")
+num_complex = complex("1")
+soma = 1 + 1 == 2
+
+print(letra)
+print(num_int)
+print(num_float)
+print(num_complex)
+print(soma)
+
