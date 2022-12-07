7cc8b99f146d4052a4e8a0f0977385b4bc9c866c
tiagolisboanovais <tiagolisboanovais@outlook.com>
2022-12-07 06:34:32 -0300

    Alterando arquivo aula 1 - Curso 1 - Módulo 1

--- 1_Fundamentos_de_Python/2_Conhecendo_a_Linguagem_de_Programacao_Python/tiposdedados.py
+++ 1_Fundamentos_de_Python/2_Conhecendo_a_Linguagem_de_Programacao_Python/tiposdedados.py
@@ -1,14 +1,31 @@
-#Tipos de dados
+#Tipos de dados simples
 
 letra = str(1)
 num_int = int("1")
 num_float = float ("1")
-num_complex = complex("1")
-soma = 1 + 1 == 2
+soma_bool = bool('1 + 1 == 2')
 
 print(letra)
 print(num_int)
 print(num_float)
-print(num_complex)
-print(soma)
+print(soma_bool)
+
+# Modo Interativo
+
+# Para acessar o programa em modo interativo basta colocar a flag -i, nesta sintax 'python -i nomeprograma.py'
+
+# Dir - Sem argumentos, retorna a lista de nomes no escopo local atual. Com um argumento, retorna uma lista de atributos válidos para o objeto. Exemplo:
+
+print(dir())
+print(dir(100))
+
+# Help - Invoca o sistema de ajuda integrado. É possível fazer buscas em modo interativo ou informar por parâmetro qual o nome do módulo, função, classe, método ou variável. Exemplo:
+# Para sair help = enter, para sair da documentação = q
+# Como se fosse documentação offline
+# Mais informações: https://wiki.python.org.br/ModoInterativo
+
+print(help())
+print(help(100))
+
+
 
